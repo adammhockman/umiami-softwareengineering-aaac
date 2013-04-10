237e9bafacacbcb775e157384bcfaaa4f8e0e411
ChristianJordanGonzalez@gmail.com <ChristianJordanGonzalez@gmail.com@3653b8d6-ef68-e2bf-55e3-0a4cf4f932c8>
2013-04-10 19:40:28 +0000

    Initial import of prototype for the database manager API. This will eventually be hosted on google app engine for this protype.

---
+++ trunk/app/DirtProsDatabase/src/edu/miami/csc531/VerifyUserServlet.java
@@ -0,0 +1,72 @@
+package edu.miami.csc531;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.QueryResultList;
+import com.google.appengine.api.datastore.Query.Filter;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
+
+public class VerifyUserServlet extends HttpServlet
+{
+	private static final long serialVersionUID = 1L;
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+	{
+		JSONObject jsonObj = new JSONObject();
+		
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		
+		// Verify the Username and Password
+	    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		Filter filter = new FilterPredicate("username", FilterOperator.EQUAL, username);
+		Query q = new Query("Emloyee").setFilter(filter);
+		PreparedQuery pq = datastore.prepare(q);
+		FetchOptions fetchOptions = FetchOptions.Builder.withLimit(1);
+		QueryResultList<Entity> results = pq.asQueryResultList(fetchOptions);
+		boolean verified = false;
+		if(results.size() > 0)
+		{
+			Entity user = results.get(0);
+			if (password.equals(user.getProperty("password")))
+			{
+				verified = true;
+			}
+		}
+		try 
+		{
+			jsonObj.put("Verified", verified);
+		}
+		catch (JSONException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// Send the response here
+		resp.setContentType("application/json");       
+	    try 
+	    {
+			jsonObj.write(resp.getWriter());
+		}
+	    catch (JSONException | IOException e) 
+	    {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
