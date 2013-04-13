849ef4be165c194f03807c95ce6f8f35991bb2ff
ChristianJordanGonzalez@gmail.com <ChristianJordanGonzalez@gmail.com@3653b8d6-ef68-e2bf-55e3-0a4cf4f932c8>
2013-04-13 16:10:32 +0000

    Modified the DirtPros database. Added ability to create logs, add bags, and display logs for a user.

---
+++ trunk/app/DirtProsDatabase/src/edu/miami/csc531/CreateCollectionLogServlet.java
@@ -0,0 +1,87 @@
+package edu.miami.csc531;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+
+public class CreateCollectionLogServlet extends HttpServlet
+{
+	 private static final long serialVersionUID = 2L;
+	 public void doPost(HttpServletRequest req, HttpServletResponse resp)
+	 {
+		 StringBuffer jb = new StringBuffer();
+		  String line = null;
+		  try {
+		    BufferedReader reader = req.getReader();
+		    while ((line = reader.readLine()) != null)
+		      jb.append(line);
+		  } catch (Exception e) { /*report an error*/ }
+
+		 JSONObject jsonObject = new JSONObject(jb.toString());
+		 String username = jsonObject.getString("username");
+		 String region = jsonObject.getString("region");
+		 String facilityType = jsonObject.getString("facility");
+		 String wastePartner = jsonObject.getString("partner");
+		 JSONArray bags = jsonObject.getJSONArray("bags");
+		 
+		  DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		  Entity collectionLog = new Entity("CollectionLog");
+		  collectionLog.setProperty("username", username);
+		  collectionLog.setProperty("region", region);
+		  collectionLog.setProperty("facility", facilityType);
+		  collectionLog.setProperty("partner", wastePartner);
+		  
+		  datastore.put(collectionLog);
+		  
+		  long collectionLogID = collectionLog.getKey().getId();
+
+		  for(int i = 0; i<bags.length(); i++)
+		  {
+			 JSONObject jsonBag = bags.getJSONObject(i);
+			 int weight = (int)jsonBag.get("weight");
+			 int wetTrashPercentage = (int)jsonBag.get("wettrash");
+			 int paperTrashPercentage = (int)jsonBag.get("papertrash");
+			 int cardboardTrashPercentage = (int)jsonBag.get("cardboardtrash");
+			 int plasticTrashPercentage = (int)jsonBag.get("plastictrash");
+			 int glassTrashPercentage = (int)jsonBag.get("glasstrash");
+			 int cansTrashPercentage = (int)jsonBag.get("canstrash");
+			 
+			 Entity bag = new Entity("Bag");
+			 bag.setProperty("logid",collectionLogID);
+			 bag.setProperty("weight", weight);
+			 bag.setProperty("wetTrashPercentage", wetTrashPercentage);
+			 bag.setProperty("paperTrashPercentage", paperTrashPercentage);
+			 bag.setProperty("cardboardTrashPercentage", cardboardTrashPercentage);
+			 bag.setProperty("plasticTrashPercentage", plasticTrashPercentage);
+			 bag.setProperty("glassTrashPercentage", glassTrashPercentage);
+			 bag.setProperty("cansTrashPercentage", cansTrashPercentage);
+			 
+			 datastore.put(bag);
+		  }
+		  
+		  JSONObject jsonResult = new JSONObject();
+		  jsonResult.put("Status", "OK");
+		  jsonResult.put("collectionLogID", collectionLogID);
+		  resp.setContentType("application/json"); 
+		  try
+		  {
+			jsonResult.write(resp.getWriter());
+		  }
+		  catch (JSONException | IOException e)
+		  {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		  }
+	 }
+}
---
+++ trunk/app/DirtProsDatabase/src/edu/miami/csc531/GetLogsServlet.java
@@ -0,0 +1,92 @@
+package edu.miami.csc531;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.QueryResultList;
+import com.google.appengine.api.datastore.Query.Filter;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
+
+public class GetLogsServlet extends HttpServlet
+{
+	private static final long serialVersionUID = 4L;
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+	{
+		  String username = req.getParameter("username");
+		 
+		  DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		  Filter filter = new FilterPredicate("username", FilterOperator.EQUAL, username);
+		  Query q = new Query("CollectionLog").setFilter(filter);
+		  PreparedQuery pq = datastore.prepare(q);
+		  FetchOptions fetchOptions = FetchOptions.Builder.withLimit(100);
+		  QueryResultList<Entity> results = pq.asQueryResultList(fetchOptions);
+		  
+		  JSONArray jsonLogArray = new JSONArray();
+		  for(Entity collectionLog : results)
+		  {
+			  long logID = collectionLog.getKey().getId();
+			  String region = (String) collectionLog.getProperty("region");
+			  String facilityType = (String) collectionLog.getProperty("facility");
+			  String wastePartner = (String)collectionLog.getProperty("partner");
+			  
+			  JSONObject collectionLogJSON = new JSONObject();
+			  collectionLogJSON.put("logID", logID);
+			  collectionLogJSON.put("region", region);
+			  collectionLogJSON.put("facility", facilityType);
+			  collectionLogJSON.put("parnter",wastePartner);
+			  
+			  Filter bagFilter = new FilterPredicate("logid", FilterOperator.EQUAL, logID);
+			  Query bagQuery = new Query("Bag").setFilter(bagFilter);
+			  PreparedQuery bagPq = datastore.prepare(bagQuery);
+			  FetchOptions bagFetchOptions = FetchOptions.Builder.withLimit(100);
+			  QueryResultList<Entity> bagResults = bagPq.asQueryResultList(bagFetchOptions);
+			  
+			  JSONArray bagArray = new JSONArray();
+			  
+			  for (Entity bag : bagResults)
+			  {
+				  JSONObject jsonBag = new JSONObject();
+				  
+				  jsonBag.put("bagID", bag.getKey().getId());
+				  jsonBag.put("weight",bag.getProperty("weight"));
+				  jsonBag.put("wetTrashPercentage",bag.getProperty("wetTrashPercentage"));
+				  jsonBag.put("paperTrashPercentage", bag.getProperty("paperTrashPercentage"));
+				  jsonBag.put("cardboardTrashPercentage",bag.getProperty("cardboardTrashPercentage"));
+				  jsonBag.put("plasticTrashPercentage",bag.getProperty("plasticTrashPercentage"));
+				  jsonBag.put("glassTrashPercentage", bag.getProperty("glassTrashPercentage"));
+				  jsonBag.put("cansTrashPercentage", bag.getProperty("cansTrashPercentage"));
+				  
+				  bagArray.put(jsonBag);
+			  }
+			  collectionLogJSON.put("Bags", bagArray);
+			  jsonLogArray.put(collectionLogJSON);
+		  }
+		  resp.setContentType("application/json"); 
+		  JSONObject jsonResult = new JSONObject();
+		  jsonResult.put("Status", "OK");
+		  jsonResult.put("Logs", jsonLogArray);
+		  try
+		  {
+			jsonResult.write(resp.getWriter());
+		  }
+		  catch (JSONException | IOException e)
+		  {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		  }
+	}
+}
