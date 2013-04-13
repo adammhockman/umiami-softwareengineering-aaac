47c3245200ef05baf0577b2789138415eacc61ff
ChristianJordanGonzalez@gmail.com <ChristianJordanGonzalez@gmail.com@3653b8d6-ef68-e2bf-55e3-0a4cf4f932c8>
2013-04-13 16:12:42 +0000

    Added the ability to upload data from the mobile app to the server.

--- app/DirtProsMobileApp/src/aaac/dirtprosmobileapp/MainOptionsScreen.java
+++ app/DirtProsMobileApp/src/aaac/dirtprosmobileapp/MainOptionsScreen.java
@@ -6,23 +6,31 @@ import android.os.Bundle;
 import android.view.View;
 //=============================================================================
 public class MainOptionsScreen extends Activity {
+	private String username; 
 //-----------------------------------------------------------------------------
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_options_screen);
+		Intent intent = getIntent();
+		Bundle extras = intent.getExtras();
+		if (extras == null)
+		{
+			return;
+		}
+		username = extras.getString("username");
 	}
 //-----------------------------------------------------------------------------
 	public void myClickHandler(View view){
 	
 		Intent nextActivity;
-		
 		switch(view.getId()){
 		case R.id.logout_button:
 			finish();
 			break;
 		case R.id.record_collection_information_button:
 			nextActivity = new Intent();
+			nextActivity.putExtra("username", username);
             nextActivity.setClassName("aaac.dirtprosmobileapp",
 "aaac.dirtprosmobileapp.RecordCollectionInfoScreen");
             startActivity(nextActivity);
